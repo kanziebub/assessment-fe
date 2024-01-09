363e4d479421b79cd80996fd3756648f8798f53b
Nabila Khansa <nabila.khansa@gmail.com>
2024-01-09 21:27:21 +0700

    feat: direct home to list and wishlist

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -24,34 +24,13 @@ const Home: NextPage = () => {
       </Head>
 
       <MainStyled>
-        <h1 className="text-3xl font-bold underline">Tailwind Header</h1>
-
-        <p className={styles.description} test-id="getting started">
-          Get started by editing <code className={styles.code}>pages/index.tsx</code>
-        </p>
-
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
+          <a href={`/planet/list`} className={styles.card}>
+            <h2>List of Planets</h2>
           </a>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a href="https://github.com/vercel/next.js/tree/master/examples" className={styles.card}>
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>Instantly deploy your Next.js site to a public URL with Vercel.</p>
+          <a href={`/planet/wishlist`} className={styles.card}>
+            <h2>Wishlisted Planets</h2>
           </a>
         </div>
       </MainStyled>
